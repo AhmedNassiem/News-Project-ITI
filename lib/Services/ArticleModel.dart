a457097a7ab999f40866148bbbfe52b44ad83dfb
AhmedNassiem <106481444+AhmedNassiem@users.noreply.github.com>
2023-08-28 14:39:44 +0300

    Update ArticleModel.dart

--- lib/Services/ArticleModel.dart
+++ lib/Services/ArticleModel.dart
@@ -13,11 +13,11 @@ class ArticleModel1 {
     required this.subTitle,
   });
 
-  // Additional constructor to create ArticleModel1 instances with filtered articles
+  
   ArticleModel1.fromFilteredArticles(List<ArticleModel1> filteredArticles)
-      : image = null, // Set appropriate value if needed
-        title = 'Filtered Articles', // Set a title for the filtered articles
-        subTitle = '', // Set appropriate value if needed
-        content = null, // Set appropriate value if needed
-        publishedAt = null; // Set appropriate value if needed
+      : image = null, 
+        title = 'Filtered Articles', 
+        subTitle = '', 
+        content = null, 
+        publishedAt = null; 
 }
